f2372574c1ed429f3867a4ff489f0b813634d0cd
Deekshit M <deekshith1404@gmail.com>
2024-11-22 19:24:21 +0530

    Created api and hooks for transactions

--- finance-tracker/app/api/[[...route]]/route.ts
+++ finance-tracker/app/api/[[...route]]/route.ts
@@ -6,6 +6,7 @@ import { zValidator } from '@hono/zod-validator';
 
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = 'edge'
 
@@ -14,7 +15,8 @@ const app = new Hono().basePath('/api')
 
 const routes = app
      .route("/accounts", accounts)
-     .route("/categories", categories);
+     .route("/categories", categories)
+     .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
